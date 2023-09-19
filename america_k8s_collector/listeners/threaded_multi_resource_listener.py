72bd007cc0604fff2bb4666365349e3818b15fc5
Emil Ksenzovsky <e.Ksenzovski@gmail.com>
2023-09-19 23:29:57 +0300

    add support for namespaced watch

--- america_k8s_collector/listeners/threaded_multi_resource_listener.py
+++ america_k8s_collector/listeners/threaded_multi_resource_listener.py
@@ -10,7 +10,7 @@ class ThreadedMultiResourceListener(MultiResourceListener):
         self._resource_listener: ResourceListener = resource_listener
         self._pool_executor: type[Executor] = ThreadPoolExecutor
 
-    def listen(self, aggregated_resources: list[AggregatedResource]) -> NoReturn:
+    def listen(self, aggregated_resources: list[AggregatedResource], namespace: str | None) -> NoReturn:
         with self._pool_executor(max_workers=len(aggregated_resources)) as executor:
             for aggregated_resource in aggregated_resources:
-                executor.submit(self._resource_listener.listen, aggregated_resource)
+                executor.submit(self._resource_listener.listen, aggregated_resource, namespace)
