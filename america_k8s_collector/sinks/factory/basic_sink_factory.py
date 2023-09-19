8124a67a3d4a8ab4b984e550c34f5ece075b8fea
Emil Ksenzovsky <e.Ksenzovski@gmail.com>
2023-09-19 18:29:06 +0300

    pass SinkConfig to SinkFactory instead of Resource

--- america_k8s_collector/sinks/factory/basic_sink_factory.py
+++ america_k8s_collector/sinks/factory/basic_sink_factory.py
@@ -1,4 +1,4 @@
-from america_k8s_collector.config.models import Resource
+from america_k8s_collector.config.models.sinks import SinkConfig
 from america_k8s_collector.sinks import Sink
 from america_k8s_collector.sinks.factory.http_sink_factory import HttpSinkFactory
 from america_k8s_collector.sinks.factory.sink_factory import SinkFactory
@@ -10,5 +10,5 @@ class BasicSinkFactory(SinkFactory):
             'http': HttpSinkFactory()
         }
 
-    def get_sink(self, resource: Resource) -> Sink:
-        return self._sink_type_to_sink_factory_map[resource.sink.type].get_sink(resource)
+    def get_sink(self, sink_config: SinkConfig) -> Sink:
+        return self._sink_type_to_sink_factory_map[sink_config.type].get_sink(sink_config)
