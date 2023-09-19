5a8271d1e3cfb8db8c48cb138dc9891b0f2885b8
Emil Ksenzovsky <e.Ksenzovski@gmail.com>
2023-09-19 18:30:10 +0300

    implement Resource-wide cache in SharedSinkFactory

--- america_k8s_collector/sinks/factory/shared_sink_factory.py
+++ america_k8s_collector/sinks/factory/shared_sink_factory.py
@@ -1,4 +1,4 @@
-from america_k8s_collector.config.models import Resource
+from america_k8s_collector.config.models.sinks import SinkConfig
 from america_k8s_collector.sinks import Sink
 from america_k8s_collector.sinks.factory.basic_sink_factory import BasicSinkFactory
 from america_k8s_collector.sinks.factory.sink_factory import SinkFactory
@@ -8,16 +8,16 @@ class SharedSinkFactory(SinkFactory):
     """
     cache the given sink factory or BasicSinkFactory if not provided with id(Resource) as cache key
     """
+
     def __init__(self, sink_factory: SinkFactory | None = None):
         self._sink_factory: SinkFactory = sink_factory or BasicSinkFactory()
-        self._shared_sinks: dict[int, Sink] = {}
+        self._shared_sinks: list[tuple[SinkConfig, Sink]] = []
 
-    def get_sink(self, resource: Resource) -> Sink:
-        # TODO: when sink property/list will be added to CollectorConfig we can cache based on sink properties and share it across resources
-        resource_ref: int = id(resource)
-        sink = self._shared_sinks.get(resource_ref)
-        if not sink:
-            sink = self._sink_factory.get_sink(resource)
-            self._shared_sinks[resource_ref] = sink
+    def get_sink(self, sink_config: SinkConfig) -> Sink:
+        for shared_sink_config, shared_sink in self._shared_sinks:
+            if sink_config == shared_sink_config:
+                return shared_sink
 
+        sink: Sink = self._sink_factory.get_sink(sink_config)
+        self._shared_sinks.append((sink_config, sink))
         return sink
