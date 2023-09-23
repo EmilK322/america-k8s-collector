f327c48cbf3d63a2b1299f3a88dc7621e449733a
Emil Ksenzovsky <e.Ksenzovski@gmail.com>
2023-09-23 05:41:57 +0300

    add option to use environment variables as HttpSink header values

--- america_k8s_collector/sinks/http/client.py
+++ america_k8s_collector/sinks/http/client.py
@@ -1,13 +1,17 @@
 import httpx
 
+from america_k8s_collector.sinks.http.headers_parsers.headers_parser import HttpSinkHeadersParser
+
 
 class HttpClient:
-    def __init__(self, url: str, headers: dict[str, str] | None = None):
+    def __init__(self, url: str, headers: dict[str, str] | None, headers_parser: HttpSinkHeadersParser):
         # not closing the connection because america-k8s-collector suppose to run indefinitely
         # and potentially a lot of resources will be changed frequently in heavy-loaded cluster
-        self._client = httpx.Client(headers=headers)
+        self._headers_parser: HttpSinkHeadersParser = headers_parser
         self._url: str = url
-        self._headers: dict[str, str] = headers
+        self._preparsed_headers: dict[str, str] | None = headers
+        self._parsed_headers: dict[str, str] | None = self._headers_parser.parse_headers(headers)
+        self._client = httpx.Client(headers=self._parsed_headers)
 
     def add(self, obj: dict) -> None:
         self._client.post(self._url, json=obj)
